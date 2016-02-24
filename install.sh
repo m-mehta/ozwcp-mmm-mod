9b085c7ee7b3bc17c3f0a4535e2d20e36189afc3
Manan <m-mehta@github.com>
2016-02-24 03:35:31 -0600

    added startup script

--- install.sh
+++ install.sh
@@ -4,3 +4,8 @@ sudo cp ./ozwcp /usr/sbin/ozwd
 sudo mkdir -p /var/ozwd
 sudo cp -r ../open-zwave/config /var/ozwd/config
 sudo cp -t /var/ozwd index.html config.xml cp.html cp.js openzwavetinyicon.png README  
+chmod 664 ozwd.service
+cp ozwd.service /etc/systemd/system/ozwd.service
+sudo systemctl daemon-reload
+sudo systemctl enable ozwd
+sudo systemctl start ozwd
